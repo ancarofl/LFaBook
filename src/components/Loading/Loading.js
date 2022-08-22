77c77a4efc26fcdf01cd8bff756c8bf68b252460
Anca Negulescu <negulescuanca11@gmail.com>
2022-08-22 11:44:03 +0200

    Add TODO regarding Loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ActivityIndicator, View } from 'react-native';
 import styles from './LoadingStyles';
 
+// TODO: Add dynamic props like size, color.
 const Loading = () => {
     return (
         <View style={styles.activityIndicatorContainer}>
