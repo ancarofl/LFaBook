5db34921510df940ac6d57a58b01fc66b046f0b6
Anca Negulescu <negulescuanca11@gmail.com>
2022-08-22 12:07:52 +0200

    WIP search - use a SearchProvider and show the titles of the books; implement infinite scroll and show number of results at the top

---
+++ src/constants/openLibraryAPI.js
@@ -0,0 +1,8 @@
+export const openLibraryAPI = {
+	defaultOffset: 0,
+    defaultLimit: 5,
+
+    // TODO: Improve how endpoints are stored. Include offset+limit as well.
+    /* Endpoints */
+    SEARCH_ENDPOINT: "https://openlibrary.org/search.json?q=",
+};
--- src/stacks/AppNav.js
+++ src/stacks/AppNav.js
@@ -3,19 +3,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Search from '../screens/Search/Search';
 import SearchResults from '../screens/SearchResults/SearchResults';
+import { SearchProvider } from '../providers/SearchProvider/SearchProvider';
 
 const Stack = createNativeStackNavigator();
 
 const AppNav = () => {
   return (
-	<Stack.Navigator
-		screenOptions={{
-			headerShown: false
-		}}
-	>
-		<Stack.Screen component={Search} name="SearchScreen"/>
-		<Stack.Screen component={SearchResults} name="SearchResultsScreen"/>
-	</Stack.Navigator>
+	<SearchProvider>
+		<Stack.Navigator
+			screenOptions={{
+				headerShown: false
+			}}
+		>
+			<Stack.Screen component={Search} name="SearchScreen"/>
+			<Stack.Screen component={SearchResults} name="SearchResultsScreen"/>
+		</Stack.Navigator>
+	</SearchProvider>
   );
 };
 
